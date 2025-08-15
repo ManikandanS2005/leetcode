217da4802f4032f719d5fa28199dd89136d8912c
Manikandan Subramanian <manikandansubramanian2005@gmail.com>
2025-08-15 23:51:44 +0530

    Time: 386 ms (87.89%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,10 @@
+WITH temp AS (
+  SELECT id, recordDate, temperature,
+         LAG(temperature) OVER (ORDER BY recordDate) AS prev_temp,
+         LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date
+  FROM Weather
+)
+SELECT id
+FROM temp
+WHERE temperature > prev_temp
+  AND DATEDIFF(recordDate, prev_date) = 1;
