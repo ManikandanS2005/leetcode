561e02f9c467049731bb96cffc55ee1f985a4e21
Manikandan Subramanian <manikandansubramanian2005@gmail.com>
2025-08-16 15:38:33 +0530

    Time: 756 ms (78.78%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,11 +1,15 @@
-WITH RunningWeight AS (
-    SELECT person_name, 
-           weight,
-           SUM(weight) OVER (ORDER BY turn) AS total_weight
-    FROM Queue
+WITH cte AS (
+  SELECT 
+    turn,
+    person_id,
+    person_name,
+    weight,
+    SUM(weight) OVER (ORDER BY turn) AS total_weight
+  FROM Queue
 )
+
 SELECT person_name
-FROM RunningWeight
+FROM cte
 WHERE total_weight <= 1000
 ORDER BY total_weight DESC
 LIMIT 1;
